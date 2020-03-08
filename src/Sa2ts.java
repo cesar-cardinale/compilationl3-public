71fcec13537ee2126c54ad9da9502a91bd229275
Baptiste <baptiste.arnoulet@etu.univ-amu.fr>
2020-03-08 14:10:36 +0100

    Tp 3 Finit

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -17,12 +17,29 @@ public class Sa2ts extends SaDepthFirstVisitor {
         return this.tableGlobale;
     }
 
+    public boolean varExist(String nom) {
+        if (tableGlobale.getVar(nom) == null && tableLocal.getVar(nom) == null) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void defaultIn(SaNode node) {
+        System.out.println("debut Passage"+node.getClass());
+    }
+
+    @Override
+    public void defaultOut(SaNode node) {
+        System.out.println("fin Passage"+node.getClass());
+    }
+
     @Override
     public Object visit(SaDecTab node) {
         defaultIn(node);
-
+        System.out.println("DecTab");
         if (tableGlobale.getVar(node.getNom()) == null){
-            tableGlobale.addVar(node.getNom(), node.tsItem.getTaille());
+                tableGlobale.addVar(node.getNom(), tableGlobale.getAdrVarCourante()+node.getTaille());
         } else {
             System.out.println("Var "+ node.getNom() + " n'existe pas");
         }
@@ -35,31 +52,41 @@ public class Sa2ts extends SaDepthFirstVisitor {
     @Override
     public Object visit(SaDecFonc node) {
         defaultIn(node);
+        System.out.println("DecFonc");
 
         this.tableLocal = new Ts();
 
-        if (tableGlobale.getFct(node.getNom()) != null) {
+        if (tableGlobale.getFct(node.getNom()) == null) {
 
-            for (TsItemVar var : tableGlobale.variables.values()) {
-                tableLocal.addVar(var.getIdentif(),var.getTaille());
+            if (node.getParametres() != null) {
+                Sa2ts sa2ts = new Sa2ts(node.getParametres());
+                Ts paramTable = sa2ts.tableGlobale;
+
+                for (TsItemVar tsItemVar : paramTable.variables.values()) {
+                    tableLocal.addParam(tsItemVar.getIdentif());
+                }
             }
 
-            Ts paramTable = new Sa2ts(node.getParametres()).tableGlobale;
+            if (node.getVariable() != null) {
+                Sa2ts sa2ts = new Sa2ts(node.getVariable());
+                Ts varTable = sa2ts.tableGlobale;
 
-            for (TsItemVar tsItemVar : paramTable.variables.values()) {
-                tableLocal.addParam(tsItemVar.getIdentif());
+                for (TsItemVar tsItemVar : varTable.variables.values()) {
+                    if (tableLocal.getVar(tsItemVar.getIdentif()) == null)
+                        tableLocal.addVar(tsItemVar.getIdentif(),tsItemVar.getTaille());
+                    else System.out.println("La même variable est déclaré en paramètre");
+                }
+
             }
-
-            Ts varTable = new Sa2ts(node.getVariable()).tableGlobale;
-
-            for (TsItemVar tsItemVar : varTable.variables.values()) {
-                tableLocal.addVar(tsItemVar.getIdentif(),tsItemVar.getTaille());
-            }
-
-            tableGlobale.addFct(node.getNom(),node.getParametres().length(),this.tableLocal, node);
+            if (node.getParametres() == null)
+                tableGlobale.addFct(node.getNom(),0,this.tableLocal, node);
+            else
+                tableGlobale.addFct(node.getNom(),node.getParametres().length(),this.tableLocal, node);
         }
 
-        node.accept(this);
+        node.getCorps().accept(this);
+
+        defaultOut(node);
 
         return null;
     }
@@ -67,11 +94,15 @@ public class Sa2ts extends SaDepthFirstVisitor {
     @Override
     public Object visit(SaDecVar node) {
         defaultIn(node);
+        System.out.println("DecVar");
 
-        if (tableGlobale.getVar(node.getNom()) == null){
-            tableGlobale.addVar(node.getNom(), node.tsItem.getTaille());
+        if (!varExist(node.getNom())){
+            if (node.tsItem == null)
+                tableGlobale.addVar(node.getNom(), tableGlobale.getAdrVarCourante()+1);
+            else
+                tableGlobale.addVar(node.getNom(), tableGlobale.getAdrVarCourante() + node.tsItem.getTaille());
         } else {
-            System.out.println("Var "+ node.getNom() + " n'existe pas");
+            System.out.println("Var "+ node.getNom() + " existe déjà");
         }
 
         defaultOut(node);
@@ -82,8 +113,9 @@ public class Sa2ts extends SaDepthFirstVisitor {
     @Override
     public Object visit(SaVarSimple node) {
         defaultIn(node);
+        System.out.println("VarSimple");
 
-        if (tableGlobale.getVar(node.getNom()) == null) {
+        if (!varExist(node.getNom())) {
             System.out.println("La var n'existe pas");
         }
 
@@ -95,9 +127,13 @@ public class Sa2ts extends SaDepthFirstVisitor {
     @Override
     public Object visit(SaAppel node) {
         defaultIn(node);
+        System.out.println("Appel");
 
         if (tableGlobale.getFct(node.getNom()) == null) {
-            System.out.println("Le tab n'existe pas");
+            System.out.println("Le tab n'existe pas!");
+        }
+        if (node.getArguments().length() != tableGlobale.getFct(node.getNom()).getNbArgs()) {
+            System.out.println("Le nombre d'argument n'est pas bon!");
         }
 
         defaultOut(node);
@@ -108,6 +144,7 @@ public class Sa2ts extends SaDepthFirstVisitor {
     @Override
     public Object visit(SaVarIndicee node) {
         defaultIn(node);
+        System.out.println("VarIndicee");
 
         if (tableGlobale.getVar(node.getNom()) == null) {
             System.out.println("Le tab n'existe pas");
